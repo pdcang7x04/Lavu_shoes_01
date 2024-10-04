5073178ab4ef4cfe2024040d5f97534940db7a87
Trần Quốc Nam <153889093+namps29861@users.noreply.github.com>
2024-10-04 20:23:47 +0700

    Create Setup_New_Password.js

---
+++ src/page/Login/Setup_New_Password.js
@@ -0,0 +1,174 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from 'react-native'
+import { mainstack } from '../../navigation/mainstack'
+import { colors } from '../../styles/colors'
+import { t } from '../../styles/font'
+import React, { useState } from 'react'
+
+const Setup_New_Password = (props) => {
+    const { navigation } = props;
+    const [NewPassword, setNewPassword] = useState('')
+    const [RepeatPassword, setRepeatPassword] = useState('')
+  return (
+    <View style={styles.container}>
+     <Image
+        source={require('../../images/icon_back.png')}
+        style={styles.iconBack}
+      />
+
+
+       <Text style={styles.textHello}>Setup New Password</Text>
+       <Text style={styles.contentHello}>Please, setup a new password for your account</Text>
+      
+
+        
+       <View style={styles.viewInput}>
+        <Text style={styles.title}> New Password</Text>
+        <View style={styles.viewTextInput}>
+          <TextInput
+            value={NewPassword}
+            onChangeText={val => setNewPassword(val)}
+            style={styles.textInput}
+            autoCapitalize='none'
+            secureTextEntry={true}
+          />
+          <TouchableOpacity>
+            <Image
+              source={require('../../images/icon_close_eye.png')}
+              style={styles.iconTextInput}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.viewInput}>
+        <Text style={styles.title}> Repeat Password</Text>
+        <View style={styles.viewTextInput}>
+          <TextInput
+            value={RepeatPassword}
+            onChangeText={val => setRepeatPassword(val)}
+            style={styles.textInput}
+            autoCapitalize='none'
+            secureTextEntry={true}
+          />
+          <TouchableOpacity>
+            <Image
+              source={require('../../images/icon_close_eye.png')}
+              style={styles.iconTextInput}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <TouchableOpacity
+        style={[styles.viewButtonSignIn, { backgroundColor: colors.orange1 }]}
+        onPress={() => navigation.navigate(mainstack.changpassword)}
+      >
+        <Text style={[styles.textButton, { color: colors.white }]}>Save</Text>
+      </TouchableOpacity>
+    
+    </View>
+  )
+}
+
+export default Setup_New_Password
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        paddingTop: 44,
+        backgroundColor: colors.white1
+      },
+      iconBack: {
+        width: 44,
+        height: 44,
+        alignSelf: 'flex-start',
+        marginTop: 8
+      },
+      textHello: {
+        fontFamily: t.Roboto_Bold,
+        fontSize: 28,
+        fontWeight: '500',
+        color: colors.black1,
+        lineHeight: 36,
+        marginTop: 32
+      },
+      contentHello: {
+        width: 273,
+        fontFamily: t.Roboto_Bold,
+        fontSize: 16,
+        color: colors.black1,
+        lineHeight: 24,
+        marginTop: 4,
+        marginBottom: 20,
+        textAlign: 'center'
+      },
+      viewInput: {
+        width: '100%',
+        marginTop: 30,
+      },
+      title: {
+        fontFamily: t.Roboto_Bold,
+        fontWeight: '500',
+        fontSize: 16,
+        color: colors.black1,
+        alignSelf: 'flex-start',
+      },
+      viewTextInput: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: colors.white,
+        borderRadius: 50,
+        paddingHorizontal: 14,
+        marginTop: 12
+      },
+      textInput: {
+        flex: 1,
+        fontFamily: t.Roboto_Bold,
+        fontWeight: '100',
+        fontSize: 14,
+        lineHeight: 16,
+        color: colors.black1
+      },
+      iconTextInput: {
+        width: 24,
+        height: 24,
+        marginRight: 8,
+        resizeMode: 'contain'
+      },
+      textForgot: {
+        fontFamily: t.Roboto_Bold,
+        fontSize: 13,
+        fontWeight: '100',
+        lineHeight: 16,
+        alignSelf: 'flex-end',
+        marginTop: 12,
+      },
+      viewButtonSignIn: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: 54,
+        borderRadius: 50,
+        marginTop: 30,
+        backgroundColor: colors.white
+      },
+      textButton: {
+        fontFamily: t.Roboto_Bold,
+        fontSize: 18,
+        fontWeight: '700',
+        lineHeight: 22,
+        color: colors.black1
+      },
+      textBottom: {
+        fontFamily: t.Roboto_Bold,
+        fontSize: 12,
+        fontWeight: '300',
+        color: colors.grey1,
+        marginBottom: 50
+      },
+      color: {
+        color: colors.orange1,
+      }
+})
