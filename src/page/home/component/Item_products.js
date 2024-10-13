38cf5143ad282e35b603fb252cc52c9ba50ec595
Trần Quốc Nam <153889093+namps29861@users.noreply.github.com>
2024-10-13 15:25:26 +0700

    Hoàn thành BestSeller,Favourite,Home

---
+++ src/page/home/component/Item_products.js
@@ -0,0 +1,35 @@
+export const popularShoes = [
+  {
+    id: '1',
+    name: 'Nike Jordan',
+    price: '$493.00',
+    image: require('../../../images/giay1.png'),
+  },
+  {
+    id: '2',
+    name: 'Nike Air Max',
+    price: '$897.99',
+    image: require('../../../images/giay2.png'),
+  },
+  {
+    id: '3',
+    name: 'Nike Air Max',
+    price: '$897.99',
+    image: require('../../../images/giay2.png'),
+  },
+  {
+    id: '3',
+    name: 'Nike Air Max',
+    price: '$897.99',
+    image: require('../../../images/giay2.png'),
+  },
+];
+
+export const newArrivals = [
+  {
+    id: '3',
+    name: 'Nike Air Jordan',
+    price: '$849.69',
+    image: require('../../../images/giay2.png'),
+  },
+];
